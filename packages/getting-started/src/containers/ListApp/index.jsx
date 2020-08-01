4dba889205b1eb22edab052f0c3b2d34c785ce93
Nguyen Phat <phatnt.uit@gmail.com>
2020-08-01 14:39:56 +0700

    update app description

--- packages/getting-started/src/containers/ListApp/index.jsx
+++ packages/getting-started/src/containers/ListApp/index.jsx
@@ -25,6 +25,23 @@ export default class ListApp extends Component {
     return (
       <div>
         <h4>List App</h4>
+        <div>
+          <pre>
+            This example include some techniques:
+            <br />
+            <span>
+              <strong>
+                <code>How to render a list ?</code>
+              </strong>
+            </span>
+            <br />
+            <span>
+              <strong>
+                <code>How to pass parameter in the callback?</code>
+              </strong>
+            </span>
+          </pre>
+        </div>
         <p>Total: {items.length} item(s)</p>
         <button onClick={this.handleAddItem}>Add new item</button>
         <ul>
