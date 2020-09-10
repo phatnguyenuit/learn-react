2c5ee6c4bd13a1fb23a04adb23a1b7470f7fdd6d
Nguyen Phat <phatnt.uit@gmail.com>
2020-09-10 23:42:09 +0700

    [hello-redux] [ADD] Add multiple-reducers exmaple

---
+++ packages/hello-redux/src/containers/MultipleReducers/index.jsx
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import { createStore, combineReducers } from 'redux';
+import './styles.css';
+
+// assign default value for prevState
+const counterReducer = (prevState = 0, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'increase': {
+      const { count = 1 } = payload;
+      return prevState + count;
+    }
+    case 'decrease': {
+      const { count = 1 } = payload;
+      return prevState - count;
+    }
+    default:
+      return prevState;
+  }
+};
+
+const toggleReducer = (prevState = false, action) => {
+  const { type } = action;
+  switch (type) {
+    case 'toggle': {
+      return !prevState;
+    }
+    default:
+      return prevState;
+  }
+};
+
+// key: reducerName
+// Store will be
+// {
+//   counter: returned counterReducer's state,
+//   toggle: returned toggleReducer's state
+// }
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  toggle: toggleReducer,
+});
+
+const store = createStore(rootReducer);
+
+class MultipleReducers extends Component {
+  constructor(props) {
+    super(props);
+    const initialState = store.getState();
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      const newState = store.getState();
+      console.log('Current state', this.state);
+      console.log('New state', newState);
+      this.setState(newState);
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
+
+  handleIncrease = () => {
+    store.dispatch({
+      type: 'increase',
+      payload: {
+        count: 1,
+      },
+    });
+  };
+
+  handleDecrease = () => {
+    store.dispatch({
+      type: 'decrease',
+      payload: {
+        count: 1,
+      },
+    });
+  };
+
+  handleDecreaseWithCount = (count = 1) => () => {
+    store.dispatch({
+      type: 'decrease',
+      payload: {
+        count,
+      },
+    });
+  };
+
+  handleIncreaseWithCount = (count = 1) => () => {
+    store.dispatch({
+      type: 'increase',
+      payload: {
+        count,
+      },
+    });
+  };
+
+  handleToggle = () => {
+    store.dispatch({
+      type: 'toggle',
+    });
+  };
+
+  render() {
+    const { counter, toggle } = this.state;
+    return (
+      <div className="app-root">
+        <h4>Show you:</h4>
+        <ul>
+          <li>How to use multiple reducers in Redux ?</li>
+        </ul>
+        <div>
+          <div className="toogle-container">
+            <label>Check: </label>
+            <input
+              type="checkbox"
+              checked={toggle}
+              onChange={this.handleToggle}
+            />
+          </div>
+          <div className="flex flex-row flex-align-center counter-root">
+            <button type="button" onClick={this.handleDecreaseWithCount(2)}>
+              --
+            </button>
+            <button type="button" onClick={this.handleDecrease}>
+              -
+            </button>
+            <span className="flex-grow text-center">{counter}</span>
+            <button type="button" onClick={this.handleIncrease}>
+              +
+            </button>
+            <button type="button" onClick={this.handleIncreaseWithCount(2)}>
+              ++
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MultipleReducers;
--- packages/hello-redux/src/containers/index.js
+++ packages/hello-redux/src/containers/index.js
@@ -1,9 +1,11 @@
 import SimpleRedux from './SimpleRedux';
 import SimpleReduxClassStyle from './SimpleReduxClassStyle';
+import MultipleReducers from './MultipleReducers';
 
 const apps = {
   'simple-redux': SimpleRedux,
   'simple-redux-class-style': SimpleReduxClassStyle,
+  'multiple-reducers': MultipleReducers,
 };
 
 export default apps;
